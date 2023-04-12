3f46a61cf47ddf2d975b024e3cae0494e1459cf2
PatrickJMurchie <gamecrafterspro@gmail.com>
2023-04-12 18:32:23 +0100

    added ub register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Register from './pages/register.js';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('home');
+
+  // render the appropriate page based on the state variable
+  let page;
+  if (currentPage === 'home') {
+    page = <HomePage />;
+  } else if (currentPage === 'login') {
+    page = <LoginPage />;
+  } else if (currentPage === 'register') {
+    page = <Register />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <ul>
+          <li><button onClick={() => setCurrentPage('home')}>Home</button></li>
+          <li><button onClick={() => setCurrentPage('login')}>Login</button></li>
+          <li><button onClick={() => setCurrentPage('register')}>Register</button></li>
+        </ul>
+      </nav>
+      {page}
     </div>
   );
+}
+
+function HomePage() {
+  return <h1>Home Page</h1>;
+}
+
+function LoginPage() {
+  return <h1>Login Page</h1>;
 }
 
 export default App;
